599dfe41f896b3c2338f5fca3257539735b20d5c
Aphiwit1 <aphiwit.boon@gmail.com>
2023-07-20 20:56:02 +0700

    delete: interface not use

--- src/components/Navbar/WithNavbar.tsx
+++ src/components/Navbar/WithNavbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ComponentProps } from './interface';
 
-const WithNavbar = (Component: React.FC<ComponentProps>) => {
+
+const WithNavbar = (Component: React.FC) => {
   const Hoc = (props:ComponentProps) => { 
     const newProps = {
         ...props,
